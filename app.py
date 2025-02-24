edb3723f24ff8f84ddacde9a69af585de3fb1e00
guillaumefrz <75796129+guillaumefrz@users.noreply.github.com>
2025-02-24 17:10:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from BlackScholes import BlackScholes
 
 from src.models.BlackScholes import BlackScholes
 from src.utils.plotting import (
